34f057be2949505b37ac6306aacfdad3b2b211f8
martin.tellez <martin.tellez@gmail.com>
2022-02-03 19:26:53 -0600

    agregando logica para log

---
+++ app/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function image(){
+        return $this->morphOne(Image::class,'imageable'); 
+    }
+}
